cbda03f547b0d6fa5e212015ea6b62e223722883
dhasuda <davihasuda@hotmail.com>
2017-05-07 06:13:08 -0300

    error correction

--- Path.py
+++ Path.py
@@ -5,7 +5,7 @@ import random
 class Path():
     def __init__(self, levels):
         self.numberOfLevels = levels
-        self.numberOfExits = (2 ** levels) - 1
+        self.numberOfExits = (2 ** levels)
         self.rightExit = random.randint(0, self.numberOfExits - 1)
 
     def plotPath:

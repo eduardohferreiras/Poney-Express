755869a64954ac54379469efba6e07e70a53779d
Eduardo Henrique <eduardo.hferreiras@gmail.com>
2017-05-09 00:24:37 -0300

    Reestruturação das pastas, adição da classe Canvas,atualização da main, criação da classe Menu e correção da classe Start_Menu.

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
+import Representations
 import pygame
 from pygame.locals import *
 
 from Engine import Player_Engine, Fork_Engine, Score_Engine
-from UIElements import Start_Menu
-
-pygame.init()
-
-
-# Canvas
-displayHeight = 1080
-displayWidth = 1920
-gameDisplay= pygame.display.set_mode((displayWidth,displayHeight),pygame.FULLSCREEN)
-pygame.display.set_caption('Pony Express')
+from GameStates import Start_Menu
+from UIElements import Canvas
 
 
-# Define colors
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
-
-
-fork = Fork_Engine.Fork(600, 700)
-score = Score_Engine.Score()
-player = Player_Engine.Player()
-
+#Se livrar dessa porra
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
@@ -31,18 +15,39 @@ class Background(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
-
 #BackGround = Background('Nevada_Desert3.jpg', [0,0])
 
-# Game starts
+
+pygame.init()
+
+#Inicialização das classes
+canvas = Canvas.Canvas(1080, 1920)
+fork = Fork_Engine.Fork(600, 700)
+score = Score_Engine.Score()
+player = Player_Engine.Player()
+startMenu = Start_Menu.Start_Menu()
+
+# Setando o display do jogo.
+canvas.set_canvas()
+
+#Setando o primeiro Game_State
+currentGameState = Representations.gameStates['ON_START_MENU']
+
+startMenu.execute_menu()
+
+#globalGameExit = False
+#while not globalGameExit:
+#    if currentGameState == Representations.gameStates["ON_START_MENU"]:
+#        currentGameState = startMenu.execute_menu()
+#    elif currentGameState == Representations.gameStates["ON_GAMEPLAY"]:
+#        currentGameState = startMenu.execute_menu()
 
 
-Start_Menu.StartMenu()
 
 
 
+#Esse gameExit deveria ser "gameplayExit" e o código abaixo deverá estar na classe gameplayDisplay(?)
 gameExit = False
-
 while not gameExit:
     for event in pygame.event.get():
         print(event)
@@ -54,16 +59,14 @@ while not gameExit:
             if event.key == K_ESCAPE:
                 gameExit = True
 
-
     # Updates
-    score.update(gameDisplay)
+    score.update(canvas.gameCanvas)
     pygame.display.update()
-    gameDisplay.fill([255, 255, 255])
+    canvas.gameCanvas.fill([255, 255, 255])
     player.movePlayer(fork)
     #gameDisplay.blit(BackGround.image, BackGround.rect)
-    gameDisplay.blit(fork.image, fork.rect)
-    gameDisplay.blit(player.image, player.rect)
-
+    canvas.gameCanvas.blit(fork.image, fork.rect)
+    canvas.gameCanvas.blit(player.image, player.rect)
 
 pygame.quit()
 quit()

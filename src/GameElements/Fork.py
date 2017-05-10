0b211a850332c3102af69f2d974b6e0ac318259f
dhasuda <davihasuda@hotmail.com>
2017-05-10 06:36:30 -0300

    comments added to Fork.py and Path.py

--- src/GameElements/Fork.py
+++ src/GameElements/Fork.py
@@ -4,7 +4,7 @@ from src import Representations
 
 
 class Fork:
-
+    # Fork is a class dedicated to the bifurcations in the game. Each  division in the path is a Fork
     def __init__(self, xStart, yStart, forkLength, forkHeight, forkState):
         self.xStart = xStart
         self.yStart = yStart
@@ -21,7 +21,7 @@ class Fork:
 
 
     def draw(self):
-        # Plots the lines of the gates
+        # Plots the lines of the gates in the screen
         pygame.draw.line(pygame.display.get_surface(), Representations.Fork_Color, (self.xStart, self.yStart), (self.xEnd, self.yEnd), 5)
         if self.forkState == Representations.forkState["DOWN"]:
             pygame.draw.line(pygame.display.get_surface(), Representations.Fork_Weak_Color, (self.xStart,self.yStart), (self.xEnd, self.yEnd + self.forkHeight), 2)
@@ -31,10 +31,13 @@ class Fork:
 
     def toggle(self):
         # Alternates gates
+        # down -> up
         if self.forkState == Representations.forkState["DOWN"]:
             self.forkState = Representations.forkState["UP"]
             self.xEnd = self.xStart + self.forkLength
             self.yEnd = self.yStart + (self.forkHeight / 2)
+
+        # up -> down
         elif self.forkState == Representations.forkState["UP"]:
             self.forkState = Representations.forkState["DOWN"]
             self.xEnd = self.xStart + self.forkLength
